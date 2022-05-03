db23d7f3bc49d3d450514a5089bc575106a060d3
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-05-02 22:43:23 -0500

    Added a test mapping to get test FooModels

--- src/main/java/dev/mayfield/scribblerabbleapi/controller/TestController.java
+++ src/main/java/dev/mayfield/scribblerabbleapi/controller/TestController.java
@@ -1,12 +1,19 @@
 package dev.mayfield.scribblerabbleapi.controller;
 
+import dev.mayfield.scribblerabbleapi.model.FooModel;
 import dev.mayfield.scribblerabbleapi.service.PostService;
 import dev.mayfield.scribblerabbleapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
 
 @Controller
 @RequestMapping("/tests")
@@ -18,6 +25,12 @@ public class TestController {
     @Autowired
     PostService postService;
 
+    @Autowired
+    private WebClient webClient;
+
+    @Value("${recourseserver.api.url}")
+    private String fooApiUrl;
+
     @GetMapping
     String getHello(Model model) {
         model.addAttribute("something", "this is coming from the controller.");
@@ -27,11 +40,23 @@ public class TestController {
         return "test";
     }
 
-
     @GetMapping("/seeAllPosts")
     String testGetPosts(Model model) {
         model.addAttribute("posts", postService.getAllPosts());
 
         return "index";
+    }
+
+    @GetMapping("/foos")
+    public String getFoos(Model model) {
+        List<FooModel> foos = this.webClient.get()
+                .uri(fooApiUrl)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<FooModel>>() {
+
+                })
+                .block();
+        model.addAttribute("foos", foos);
+        return "foos";
     }
 }
