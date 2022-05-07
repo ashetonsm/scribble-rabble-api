87b7a2f9ebe1eb7ca31bebe65c46914a4f9140d6
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-05-07 17:08:02 -0500

    Added documentation, removed imports

--- src/main/java/dev/mayfield/scribblerabbleapi/controller/TestController.java
+++ src/main/java/dev/mayfield/scribblerabbleapi/controller/TestController.java
@@ -1,21 +1,13 @@
 package dev.mayfield.scribblerabbleapi.controller;
 
-import dev.mayfield.scribblerabbleapi.model.FooModel;
 import dev.mayfield.scribblerabbleapi.service.PostService;
 import dev.mayfield.scribblerabbleapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import javax.annotation.security.RolesAllowed;
-import java.lang.reflect.ParameterizedType;
-import java.util.List;
 
 @Controller
 @RequestMapping("/tests")
@@ -44,6 +36,13 @@ public class TestController {
     }
 
     // TODO: Set to "admin" only with @RolesAllowed("admin")
+
+    /**
+     * Can only be accessed by sending a GET request with a valid authorization token in the header
+     * @param model
+     * @param token
+     * @return
+     */
     @GetMapping("/adminPortal")
     String adminPortal(Model model, @RequestHeader (name="Authorization") String token) {
         System.out.println(token);
