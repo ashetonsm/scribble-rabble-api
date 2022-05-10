a55daca26fe95f36794fb3d34ebfa6f96620ae14
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-05-09 23:54:56 -0500

    Moved to static resources

--- src/main/resources/static/javascript/canvas.js
+++ src/main/resources/static/javascript/canvas.js
@@ -1,4 +1,5 @@
 const canvas = document.getElementById('canvas');
+const submitButton = document.getElementById('submitPost');
 const context = canvas.getContext('2d');
 context.fillStyle = 'black';
 let isDrawing = false;
@@ -12,18 +13,28 @@ const draw = (e) => {
     context.stroke();
 }
 
+//Send POST request to /posts/upload.
+// Requires Request Params "file" (multipart file), "author" (string)
+function submitPost() {
+    var canvas = document.getElementById('canvas');
+    document.getElementById("image").value = canvas.toDataURL();
+    console.log(document.getElementById("image").value);
+    console.log("Post submitted.");
+}
+
 canvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
     console.log("Started a stroke.");
     context.fillRect(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop, 2, 2);
 
-})
+});
 
 canvas.addEventListener('mouseup', (e) => {
     console.log("Ended a stroke.");
     isDrawing = false;
     context.stroke();
     context.beginPath();
-})
+});
 
 canvas.addEventListener('mousemove', draw);
+submitButton.addEventListener('click', submitPost);
