0b813be5173403f2d99018e973a3d4d08be64fc1
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-05-11 21:43:45 -0500

    Get access token from security context

--- src/main/java/dev/mayfield/scribblerabbleapi/controller/MainController.java
+++ src/main/java/dev/mayfield/scribblerabbleapi/controller/MainController.java
@@ -1,10 +1,15 @@
 package dev.mayfield.scribblerabbleapi.controller;
 
 import dev.mayfield.scribblerabbleapi.service.PostService;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.AccessToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class MainController {
@@ -13,9 +18,16 @@ public class MainController {
     PostService postService;
 
     @GetMapping("/")
-    String returnIndex(Model model) {
-        model.addAttribute("posts", postService.getAllPosts());
+    String returnIndex(Model model, Principal principal) {
+        KeycloakAuthenticationToken keycloakAuthenticationToken = (KeycloakAuthenticationToken)
+                SecurityContextHolder.getContext().getAuthentication();
+        AccessToken token = keycloakAuthenticationToken.getAccount().getKeycloakSecurityContext().getToken();
 
+        // Current user
+        System.out.println(token.getPreferredUsername());
+
+        model.addAttribute("posts", postService.getAllPosts());
+        model.addAttribute("currentUser", token.getPreferredUsername());
         return "index";
     }
 }
